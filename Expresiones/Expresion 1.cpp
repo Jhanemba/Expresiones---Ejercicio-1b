c0ff47974a9a39dc4964ad68f46077be353578bf
Kevin Eduardo Cardenas mamani <kevin.kc541@gmail.com>
2022-06-24 11:29:48 -0400

    Agregar archivos de proyecto.

---
+++ Expresiones/Expresion 1.cpp
@@ -0,0 +1,24 @@
+/*
+Ejercicio 1: Escribe la siguiente expresión como expresión en C++:
+"a" sobre "b" más uno.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	double resultado = 0;
+
+	cout << "\n\nEste programa realizara la siguiente expresion: a/b + 1 \n\n";
+	cout << "Introduzca a: \n"; cin >> a;
+	cout << "Introduzca b: \n"; cin >> b;
+
+	resultado = (a / b) + 1;
+
+	cout << "\nEl resultado es: " << resultado << ".";
+
+
+	return 0;
+}
